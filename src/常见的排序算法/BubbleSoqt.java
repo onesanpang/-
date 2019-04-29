634e215bedde6253cda08e534efc93a2630550e3
onesanpang <2393673281qq.com>
2019-04-29 21:47:21 +0800

    完成冒泡算法

--- src/常见的排序算法/BubbleSoqt.java
+++ src/常见的排序算法/BubbleSoqt.java
@@ -1,5 +1,21 @@
 package 常见的排序算法;
 
 public class BubbleSoqt {
+    public static void main(String[] args) {
+        int[] a = {9,3,5,4,2,1,6,7,8};
+        for(int i=0;i<a.length;i++){
+            for(int j=0;j<a.length-i-1;j++){
+                if (a[j]>a[j+1]){
+                    int temp = a[j+1];
+                    a[j+1] = a[j];
+                    a[j] = temp;
+                }
+            }
+        }
+
+        for (int i: a){
+            System.out.println(i);
+        }
+    }
 
 }
